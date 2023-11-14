dae39918054e9cc9c3b649064090789ba149b3fc
Silaswalter <129095844+Silaswalter@users.noreply.github.com>
2023-11-13 20:54:22 -0600

    starterdatabase

--- app/src/main/java/com/example/mysimpletestapplication/AddNote.kt
+++ app/src/main/java/com/example/mysimpletestapplication/AddNote.kt
@@ -5,23 +5,25 @@ import android.os.Bundle
 import android.widget.Toast
 import com.example.mysimpletestapplication.databinding.ActivityAddNoteBinding
 
-private lateinit var binding: ActivityAddNoteBinding
-private lateinit var db: NotesDatabaseHelper
+class AddNote : AppCompatActivity() {
 
-override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    binding = ActivityAddNoteBinding.inflate(layoutInflater)
-    setContentView(binding.root)
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db: NotesDatabaseHelper
 
-    db = NotesDatabaseHelper(this)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    binding.saveButton.setOnClickListener {
-        val title = binding.titleEditText.text.toString()
-        val content = binding.contentEditText.text.toString()
-        val note = Note(0, title, content)
-        db.insertNote(note)
-        finish()
-        Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show()
+        db = NotesDatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener {
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val note = Note(0, title, content)
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show()
+        }
     }
-}
 }
--- app/src/main/java/com/example/mysimpletestapplication/DataViewMain.kt
+++ app/src/main/java/com/example/mysimpletestapplication/DataViewMain.kt
@@ -4,16 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.mysimpletestapplication.databinding.ActivityMainBinding
+import com.example.mysimpletestapplication.databinding.ActivityDataViewMainBinding
 
 class DataViewMain : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityDataViewMainBinding
     private lateinit var db:NotesDatabaseHelper
     private lateinit var notesAdapter: NotesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityDataViewMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         db = NotesDatabaseHelper(this)
@@ -23,7 +23,7 @@ class DataViewMain : AppCompatActivity() {
         binding.notesRecyclerView.adapter = notesAdapter
 
         binding.addButton.setOnClickListener {
-            val intent = Intent(this, AddNoteActivity::class.java )
+            val intent = Intent(this, AddNote::class.java )
             startActivity(intent)
         }
 
