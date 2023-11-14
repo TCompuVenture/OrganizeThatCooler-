0362ac8968775b63263ed31e27a2e61a6ba62b71
TCompuVenture <blazingpipes@gmail.com>
2023-11-14 14:23:18 -0600

    Removed back button on "Access note application" screen b/c functionality now implemented in correct place.

--- app/src/main/java/com/example/mysimpletestapplication/DataViewMain.kt
+++ app/src/main/java/com/example/mysimpletestapplication/DataViewMain.kt
@@ -3,6 +3,7 @@ package com.example.mysimpletestapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mysimpletestapplication.databinding.ActivityDataViewMainBinding
@@ -23,10 +24,13 @@ class DataViewMain : AppCompatActivity() {
         binding.notesRecyclerView.layoutManager = LinearLayoutManager(this)
         binding.notesRecyclerView.adapter = notesAdapter
 
-        binding.addButton.setOnClickListener {
-            val intent = Intent(this, AddNote::class.java )
-            startActivity(intent)
-        }
+
+//        binding.addButton.setOnClickListener {
+//            val intent = Intent(this, AddNote::class.java )
+//            startActivity(intent)
+//        }
+        //Hides the add button from the view notes screen so that user isn't confused. Can't delete for some reason, but hey! It's gone!
+        binding.addButton.setVisibility(View.GONE);
 
         var backButton: Button = findViewById<Button>(R.id.backButton)
         backButton.setOnClickListener {
