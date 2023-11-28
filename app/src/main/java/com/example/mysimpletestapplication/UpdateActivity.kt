77471d03a887b7146f212dd614064ff06a27d3b2
Silaswalter <129095844+Silaswalter@users.noreply.github.com>
2023-11-28 16:57:26 -0600

    update goes to add screen

--- app/src/main/java/com/example/mysimpletestapplication/UpdateActivity.kt
+++ app/src/main/java/com/example/mysimpletestapplication/UpdateActivity.kt
@@ -3,17 +3,17 @@ package com.example.mysimpletestapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.example.mysimpletestapplication.databinding.UpdateItemOldDropBinding
+import com.example.mysimpletestapplication.databinding.ActivityAddItemBinding
 
 class UpdateActivity : AppCompatActivity() {
 
-    private lateinit var binding: UpdateItemOldDropBinding
+    private lateinit var binding: ActivityAddItemBinding
     private lateinit var db: ItemDatabaseHelper
     private var noteId: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = UpdateItemOldDropBinding.inflate(layoutInflater)
+        binding = ActivityAddItemBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         db = ItemDatabaseHelper(this)
@@ -25,12 +25,14 @@ class UpdateActivity : AppCompatActivity() {
         }
 
         val note = db.getNoteByID(noteId)
-        binding.updateTitleEditText.setText(note.title)
-        binding.updateContentEditText.setText(note.upc.toString())
+        binding.titleEditText.setText(note.title)
+        binding.contentEditText.setText(note.upc.toString())
 
-        binding.updateSaveButton.setOnClickListener {
-            val newTitle = binding.updateTitleEditText.text.toString()
-            val newUPCtemp = binding.updateContentEditText.text.toString()
+
+
+        binding.saveButton.setOnClickListener {
+            val newTitle = binding.titleEditText.text.toString()
+            val newUPCtemp = binding.contentEditText.text.toString()
             val newUPC = newUPCtemp.toLong()
             val updateItem = Item(noteId, newTitle, newUPC, 1) //Passing default value for now
             db.updateItem(updateItem)
